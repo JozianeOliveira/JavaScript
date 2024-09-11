3e61b6084c9d4d314e147cb3671cedef5277d737
Joziane Oliveira <jozioliveira@edu.uniube.br>
2024-09-11 09:00:29 -0300

    If Else

---
+++ exe006.js
@@ -0,0 +1,14 @@
+var vel = 75.3
+console.log(`A velocidade do seu carro é ${vel} km/h`)
+if (vel > 60) {
+    console.log('Você ultrapassou a velocidade permitida! Multado!')
+}
+console.log('Dirija sempre usando o cinto de segurança!')
+
+var pais = 'EUA'
+console.log(`Vivendo em ${pais}`)
+if (pais == 'Brasil') {
+    console.log('Vc é brasileiro!')
+} else {
+    console.log('Vc é estrangeiro!')
+}
